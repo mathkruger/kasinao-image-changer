d55ea98c0cef05adb6eaab80e706f30631ddfa07
Matheus Kruger <matheus.silva@datapar.com.br>
2021-01-26 08:39:21 -0300

    removendo tentativa de voltar as imagens originais, eles que lutem

--- content.js
+++ content.js
@@ -6,45 +6,21 @@ const colocaKasino = () => {
     });
 }
 
-const tiraKasino = () => {
-    // let imagens = document.querySelectorAll('img');
-
-    // imagens.forEach(item => {
-    //     item.src = 'https://images.uncyc.org/pt/thumb/1/11/Kasinao.png/250px-Kasinao.png';
-    // });
-
-    // num sei oq fazer
-}
-
 const addListeners = () => {
     colocaKasino();
 
-    window.addEventListener("scroll", () => {
-        colocaKasino();
-    });
+    window.addEventListener("scroll", colocaKasino);
 
     let imagens = document.querySelectorAll('img');
     imagens.forEach(item => {
-        hover(item, e => {
-            colocaKasino();
-        }, e => {
-            colocaKasino();
-        })
+        element.addEventListener('mouseenter', colocaKasino)
     });
-}
-
-function hover(element, enter, leave) {
-    element.addEventListener('mouseenter', enter)
-    element.addEventListener('mouseleave', leave)
 }
 
 //message listener for background
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.command === 'init') {
         addListeners();
-    }
-    else {
-        tiraKasino();
     }
 
     sendResponse({ result: "success" });
@@ -55,8 +31,6 @@ window.onload = function () {
     chrome.storage.sync.get('kasino', function (data) {
         if (data.kasino) {
             addListeners();
-        } else {
-            tiraKasino();
         }
     });
 }
--- mudaKasino.js
+++ mudaKasino.js
@@ -1,28 +1,9 @@
-var mudaKasino = document.getElementById('kasino');
-
-chrome.storage.sync.get('kasino', function (data) {
-    mudaKasino.checked = data.kasino;
+chrome.storage.sync.set({ 'kasino': value }, function () {
+    console.log('The value is ' + value);
 });
 
-mudaKasino.onchange = function (element) {
-    let value = this.checked;
-
-    chrome.storage.sync.set({ 'kasino': value }, function () {
-        console.log('The value is ' + value);
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { command: "init", hide: value }, function (response) {
+        console.log(response);
     });
-
-    if (value) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { command: "init", hide: value }, function (response) {
-                console.log(response.result);
-            });
-        });
-    }
-    else {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { command: "remove", hide: value }, function (response) {
-                console.log(response.result);
-            });
-        });
-    }
-};
+});
